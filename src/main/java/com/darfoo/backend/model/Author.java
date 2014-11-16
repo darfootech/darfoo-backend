b92aa579dc64988a1bf3737669633cc7ab6a6aa9
zjh <zjh@cleantha.local>
2014-11-16 16:56:37 +0800

    finish the author part

--- src/main/java/com/darfoo/backend/model/Author.java
+++ src/main/java/com/darfoo/backend/model/Author.java
@@ -4,4 +4,37 @@ package com.darfoo.backend.model;
  * Created by zjh on 14-11-16.
  */
 public class Author {
+    Integer id;
+    String name;
+    String description;
+
+    public Author() {
+        this.id = 3;
+        this.name = "ccc";
+        this.description = "this is a composer";
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
