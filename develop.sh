d3fe10d3433cfc0f3e7d47759cf93df6b7eedb90
zjh <zjh@cleantha.local>
2014-11-25 14:17:34 +0800

    finish single video and update develpment.sh

--- develop.sh
+++ develop.sh
@@ -1,5 +1,7 @@
 ## just for the development env to automatic deploy the project on localhost
-mvn package
+rm -rf target/
+mvn package -Dmaven.test.skip=true
+#mvn package
 rm -rf /Library/apache-tomcat-7.0.56/webapps/darfoobackend
 rm /Library/apache-tomcat-7.0.56/webapps/darfoobackend.war
 cp target/darfoobackend.war /Library/apache-tomcat-7.0.56/webapps
--- src/main/java/com/darfoo/backend/service/VideoController.java
+++ src/main/java/com/darfoo/backend/service/VideoController.java
@@ -1,7 +1,8 @@
 package com.darfoo.backend.service;
 
+import com.darfoo.backend.dao.VideoDao;
 import com.darfoo.backend.model.Video;
-import com.darfoo.backend.model.VideoCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +17,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/resources/video")
 public class VideoController {
     //http://localhost:8080/darfoobackend/rest/resources/video/3
+    @Autowired
+    VideoDao videoDao;
+
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public @ResponseBody
-    Video getSingleVideo(@PathVariable String id){
-        Video targetVideo = new Video();
+    SingleVideo getSingleVideo(@PathVariable String id){
+        Video targetVideo = videoDao.getVideoByVideoId(Integer.parseInt(id));
         //targetVideo.setCategories(new VideoCategory[] { new VideoCategory(), new VideoCategory(), new VideoCategory() });
-        return targetVideo;
+        int videoid = targetVideo.getId();
+        String video_url = targetVideo.getVideo_key();
+        String title = targetVideo.getTitle();
+        return new SingleVideo(videoid, title, video_url);
     }
 
     @RequestMapping(value = "/playurl/{key}", method = RequestMethod.GET)
