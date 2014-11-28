aa0ad14dc7f899a2e7a519ee20709f0f25369586
zjh <zjh@cleantha.local>
2014-11-28 09:51:39 +0800

    make jsp works

---
+++ src/main/java/com/darfoo/backend/service/UploadController.java
@@ -0,0 +1,24 @@
+package com.darfoo.backend.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by zjh on 14-11-27.
+ * 用于上传伴奏，视频和教学视频
+ */
+
+@Controller
+public class UploadController {
+    @RequestMapping(value = "/resources/video/new", method = RequestMethod.GET)
+    public String uploadVideo(ModelMap modelMap){
+        modelMap.addAttribute("message", "cleantha");
+        return "hello";
+    }
+}
