ae555dedc39ad75173ed8bfe97f8f1379dd3060a
zjh <zjh@cleantha.local>
2014-11-29 13:12:44 +0800

    折腾了一上午，把上传到服务器，服务器再上传到七牛搞通了，就是需要一个中间文件夹，删除再创建，节省阿里云上的磁盘空间

--- src/main/java/com/darfoo/backend/service/UploadController.java
+++ src/main/java/com/darfoo/backend/service/UploadController.java
@@ -1,13 +1,23 @@
 package com.darfoo.backend.service;
 
+import com.darfoo.backend.utils.FileUtils;
+import com.darfoo.backend.utils.QiniuUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.*;
+import java.util.Date;
+import java.util.Iterator;
 
 /**
  * Created by zjh on 14-11-27.
@@ -16,6 +26,8 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 public class UploadController {
+    QiniuUtils qiniuUtils = new QiniuUtils();
+
     @RequestMapping(value = "/resources/video/new", method = RequestMethod.GET)
     public String uploadVideo(ModelMap modelMap, HttpSession session){
         session.setAttribute("resource", "video");
@@ -107,5 +119,108 @@ public class UploadController {
         Long update_timestamp = System.currentTimeMillis() / 1000;
         System.out.println("requests: " + name + " " + description + " " + imagekey + " " + update_timestamp);
         return "cleantha";
+    }
+
+    @RequestMapping(value = "startupload", method = RequestMethod.GET)
+    public String startUpload(){
+        return "uploadfile";
+    }
+
+    /*
+     * 通过流的方式上传文件
+     * @RequestParam("file") 将name=file控件得到的文件封装成CommonsMultipartFile 对象
+     */
+    @RequestMapping("/uploadfile1")
+    public String  fileUpload(@RequestParam("file") CommonsMultipartFile file) throws IOException {
+        //用来检测程序运行时间
+        long  startTime=System.currentTimeMillis();
+        System.out.println("fileName："+file.getOriginalFilename());
+
+        try {
+            //获取输出流
+            OutputStream os=new FileOutputStream("./"+new Date().getTime()+file.getOriginalFilename());
+            //获取输入流 CommonsMultipartFile 中可以直接得到文件的流
+            InputStream is= file.getInputStream();
+            int temp;
+            //一个一个字节的读取并写入
+            while((temp=is.read())!=(-1))
+            {
+                os.write(temp);
+            }
+            os.flush();
+            os.close();
+            is.close();
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        long  endTime=System.currentTimeMillis();
+        System.out.println("方法一的运行时间："+String.valueOf(endTime-startTime)+"ms");
+        return "/success";
+    }
+
+    /*
+     * 采用file.Transto 来保存上传的文件
+     */
+    @RequestMapping("/uploadfile2")
+    public String  fileUpload2(@RequestParam("file") CommonsMultipartFile file) throws IOException {
+        //删除目录
+        String fileDir = "./uploadfiles";
+        FileUtils.delete(fileDir);
+
+        long  startTime=System.currentTimeMillis();
+        System.out.println("fileName："+file.getOriginalFilename());
+
+        //创建目录
+        String dirName = "./uploadfiles";
+        FileUtils.createDir(dirName);
+        String path="./uploadfiles/" + file.getOriginalFilename();
+
+        File newFile=new File(path);
+        //通过CommonsMultipartFile的方法直接写文件（注意这个时候）
+        file.transferTo(newFile);
+
+        String statusCode = qiniuUtils.uploadResouce(path, file.getOriginalFilename());
+        System.out.println("status code: " + statusCode);
+
+        long  endTime=System.currentTimeMillis();
+        System.out.println("方法二的运行时间："+String.valueOf(endTime-startTime)+"ms");
+        return "/success";
+    }
+
+    /*
+     *采用spring提供的上传文件的方法
+     */
+    @RequestMapping("/uploadfile3")
+    public String  springUpload(HttpServletRequest request) throws IllegalStateException, IOException
+    {
+        long  startTime=System.currentTimeMillis();
+        //将当前上下文初始化给  CommonsMutipartResolver （多部分解析器）
+        CommonsMultipartResolver multipartResolver=new CommonsMultipartResolver(
+                request.getSession().getServletContext());
+        //检查form中是否有enctype="multipart/form-data"
+        if(multipartResolver.isMultipart(request))
+        {
+            //将request变成多部分request
+            MultipartHttpServletRequest multiRequest=(MultipartHttpServletRequest)request;
+            //获取multiRequest 中所有的文件名
+            Iterator iter=multiRequest.getFileNames();
+
+            while(iter.hasNext())
+            {
+                //一次遍历所有文件
+                MultipartFile file=multiRequest.getFile(iter.next().toString());
+                if(file!=null)
+                {
+                    String path="./"+file.getOriginalFilename();
+                    //上传
+                    file.transferTo(new File(path));
+                }
+            }
+        }
+        long  endTime=System.currentTimeMillis();
+        System.out.println("方法三的运行时间："+String.valueOf(endTime-startTime)+"ms");
+        return "/success";
     }
 }
---
+++ src/main/webapp/WEB-INF/pages/success.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zjh
+  Date: 14-11-29
+  Time: 上午10:59
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+    上传成功
+
+</body>
+</html>
