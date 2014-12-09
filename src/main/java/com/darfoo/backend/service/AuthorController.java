01e49d1546df8888ba889c9bedadf643c064f34b
zjh <zjh@cleantha.local>
2014-12-09 16:54:06 +0800

    增加了首页明星舞队的api 增加了获取单个明星舞队(作者)的api

--- src/main/java/com/darfoo/backend/service/AuthorController.java
+++ src/main/java/com/darfoo/backend/service/AuthorController.java
@@ -4,20 +4,59 @@ package com.darfoo.backend.service;
  * Created by zjh on 14-11-16.
  */
 
+import com.darfoo.backend.dao.AuthorDao;
 import com.darfoo.backend.model.Author;
+import com.darfoo.backend.service.responsemodel.IndexAuthor;
+import com.darfoo.backend.utils.QiniuUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/resources/author")
 public class AuthorController {
+    @Autowired
+    AuthorDao authorDao;
+
+    QiniuUtils qiniuUtils = new QiniuUtils();
+
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public @ResponseBody
     Author getSingleAuthor(@PathVariable String id){
-        Author targetAuthor = new Author();
+        Author targetAuthor = authorDao.getAuthor(Integer.parseInt(id));
         return targetAuthor;
+    }
+
+    @RequestMapping("/index")
+    public
+    @ResponseBody
+    List<IndexAuthor> getIndexAuthor(){
+        List<Author> authors = authorDao.getAllAuthor();
+
+        /*int returnCount = 7;
+        List<Author> returnAuthors = new ArrayList<Author>();
+        for (int i=0; i<returnCount; i++){
+            returnAuthors.add(authors.get(i));
+        }*/
+
+        List<IndexAuthor> result = new ArrayList<IndexAuthor>();
+        for (Author author : authors){
+            int id = author.getId();
+            String image_url = "";
+            if (author.getImage() != null){
+                image_url = author.getImage().getImage_key();
+            }
+            String image_download_url = qiniuUtils.getQiniuResourceUrl(image_url);
+            String title = author.getName();
+            String description = author.getDescription();
+            result.add(new IndexAuthor(id, image_download_url, title, description));
+        }
+        return result;
     }
 }
