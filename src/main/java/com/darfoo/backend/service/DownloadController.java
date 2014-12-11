f79548e7c075c6e8fb6eefd42a67c8c1892f4c15
zjh <zjh@cleantha.local>
2014-12-11 21:26:45 +0800

    完成了视频教程伴奏三种资源的csv导出

--- src/main/java/com/darfoo/backend/service/DownloadController.java
+++ src/main/java/com/darfoo/backend/service/DownloadController.java
@@ -1,8 +1,9 @@
 package com.darfoo.backend.service;
 
+import com.darfoo.backend.dao.EducationDao;
+import com.darfoo.backend.dao.MusicDao;
 import com.darfoo.backend.dao.VideoDao;
-import com.darfoo.backend.model.Video;
-import com.darfoo.backend.model.VideoCategory;
+import com.darfoo.backend.model.*;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.io.FileUtils;
@@ -32,6 +33,10 @@ import java.util.Set;
 public class DownloadController {
     @Autowired
     VideoDao videoDao;
+    @Autowired
+    EducationDao educationDao;
+    @Autowired
+    MusicDao musicDao;
 
     public void writeVideosToCSV(){
         List<Video> videos = videoDao.getAllVideo();
@@ -62,34 +67,34 @@ public class DownloadController {
                         System.out.println("something is wrong with the category");
                     }
                 }
-                List<String> videoData = new ArrayList<String>();
-                videoData.add(video.getTitle());
+                List<String> itemData = new ArrayList<String>();
+                itemData.add(video.getTitle());
                 if (video.getAuthor() != null) {
-                    videoData.add(video.getAuthor().getName());
+                    itemData.add(video.getAuthor().getName());
                 }else{
-                    videoData.add("没有关联明星舞队");
+                    itemData.add("没有关联明星舞队");
                 }
                 if (styleMap.get("speed") != null) {
-                    videoData.add(styleMap.get("speed"));
+                    itemData.add(styleMap.get("speed"));
                 }else{
-                    videoData.add("没有填视频速度");
+                    itemData.add("没有填视频速度");
                 }
                 if (styleMap.get("difficult") != null) {
-                    videoData.add(styleMap.get("speed"));
+                    itemData.add(styleMap.get("difficult"));
                 }else{
-                    videoData.add("没有填视频速度");
+                    itemData.add("没有填视频难度");
                 }
                 if (styleMap.get("style") != null) {
-                    videoData.add(styleMap.get("speed"));
+                    itemData.add(styleMap.get("style"));
                 }else{
-                    videoData.add("没有填视频速度");
+                    itemData.add("没有填视频风格");
                 }
                 if (styleMap.get("letter") != null) {
-                    videoData.add(styleMap.get("speed"));
+                    itemData.add(styleMap.get("letter"));
                 }else{
-                    videoData.add("没有填视频速度");
+                    itemData.add("没有填首字母");
                 }
-                printer.printRecord(videoData);
+                printer.printRecord(itemData);
             }
             //close the printer
             printer.flush();
@@ -102,11 +107,160 @@ public class DownloadController {
     }
 
     @RequestMapping(value = "downloadvideos", method = RequestMethod.GET)
-    public ResponseEntity<byte[]> download() throws IOException {
-        String path="D:\\workspace\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\springMVC\\WEB-INF\\upload\\图片10（定价后）.xlsx";
-        File file=new File(path);
+    public ResponseEntity<byte[]> downloadvideos() throws IOException {
+        writeVideosToCSV();
+
+        String path = "video.csv";
+        File file = new File(path);
         HttpHeaders headers = new HttpHeaders();
-        String fileName=new String("你好.xlsx".getBytes("UTF-8"),"iso-8859-1");//为了解决中文名称乱码问题
+        String fileName = new String("video.csv".getBytes("UTF-8"), "iso-8859-1");//为了解决中文名称乱码问题
+        headers.setContentDispositionFormData("attachment", fileName);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),
+                headers, HttpStatus.CREATED);
+    }
+
+    public void writeTutorialsToCSV(){
+        List<Education> educations = educationDao.getAllEducation();
+        CSVFormat format = CSVFormat.RFC4180.withHeader().withDelimiter(',');
+        CSVPrinter printer = null;
+        try {
+            Writer out = new FileWriter("tutorial.csv");
+            printer = new CSVPrinter(out, format.withDelimiter(','));
+            System.out.println("********");
+            printer.printRecord("教程标题", "明星舞队名称", "舞蹈速度", "舞蹈难度", "舞蹈风格");
+            for(Education tutorial : educations){
+                HashMap<String, String> styleMap = new HashMap<String, String>();
+                Set<EducationCategory> categories = tutorial.getCategories();
+                for (EducationCategory category : categories){
+                    int categoryid = category.getId();
+                    String categorytitle = category.getTitle();
+                    System.out.println(categoryid);
+                    System.out.println(categorytitle);
+                    if (categorytitle.equals("快") || categorytitle.equals("中") || categorytitle.equals("慢")){
+                        styleMap.put("speed", categorytitle);
+                    }else if(categorytitle.equals("简单") || categorytitle.equals("适中") || categorytitle.equals("稍难")){
+                        styleMap.put("difficult", categorytitle);
+                    }else if(categorytitle.equals("背面教学") || categorytitle.equals("分解教学") || categorytitle.equals("队形表演")){
+                        styleMap.put("style", categorytitle);
+                    }else{
+                        System.out.println("something is wrong with the category");
+                    }
+                }
+                List<String> itemData = new ArrayList<String>();
+                itemData.add(tutorial.getTitle());
+                if (tutorial.getAuthor() != null) {
+                    itemData.add(tutorial.getAuthor().getName());
+                }else{
+                    itemData.add("没有关联明星舞队");
+                }
+                if (styleMap.get("speed") != null) {
+                    itemData.add(styleMap.get("speed"));
+                }else{
+                    itemData.add("没有填教程速度");
+                }
+                if (styleMap.get("difficult") != null) {
+                    itemData.add(styleMap.get("difficult"));
+                }else{
+                    itemData.add("没有填教程难度");
+                }
+                if (styleMap.get("style") != null) {
+                    itemData.add(styleMap.get("style"));
+                }else{
+                    itemData.add("没有填教程风格");
+                }
+
+                printer.printRecord(itemData);
+            }
+            //close the printer
+            printer.flush();
+            printer.close();
+            //out.flush();
+            //out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping(value = "downloadtutorials", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> downloadtutorials() throws IOException {
+        writeTutorialsToCSV();
+
+        String path = "tutorial.csv";
+        File file = new File(path);
+        HttpHeaders headers = new HttpHeaders();
+        String fileName = new String("tutorial.csv".getBytes("UTF-8"), "iso-8859-1");//为了解决中文名称乱码问题
+        headers.setContentDispositionFormData("attachment", fileName);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),
+                headers, HttpStatus.CREATED);
+    }
+
+    public void writeMusicsToCSV(){
+        List<Music> musics = musicDao.getAllMusic();
+        CSVFormat format = CSVFormat.RFC4180.withHeader().withDelimiter(',');
+        CSVPrinter printer = null;
+        try {
+            Writer out = new FileWriter("music.csv");
+            printer = new CSVPrinter(out, format.withDelimiter(','));
+            System.out.println("********");
+            printer.printRecord("伴奏标题", "舞蹈节奏", "舞蹈风格", "首字母");
+            for(Music music : musics){
+                HashMap<String, String> styleMap = new HashMap<String, String>();
+                Set<MusicCategory> categories = music.getCategories();
+                for (MusicCategory category : categories){
+                    int categoryid = category.getId();
+                    String categorytitle = category.getTitle();
+                    System.out.println(categoryid);
+                    System.out.println(categorytitle);
+                    if (categoryid >= 1 && categoryid <= 4){
+                        styleMap.put("beat", categorytitle);
+                    }else if(categoryid >= 5 && categoryid <= 12){
+                        styleMap.put("style", categorytitle);
+                    }else if(categoryid >= 13 && categoryid <= 38){
+                        styleMap.put("letter", categorytitle);
+                    }else{
+                        System.out.println("something is wrong with the category");
+                    }
+                }
+                List<String> itemData = new ArrayList<String>();
+                itemData.add(music.getTitle());
+
+                if (styleMap.get("beat") != null) {
+                    itemData.add(styleMap.get("beat"));
+                }else{
+                    itemData.add("没有填舞蹈节奏");
+                }
+                if (styleMap.get("style") != null) {
+                    itemData.add(styleMap.get("style"));
+                }else{
+                    itemData.add("没有填舞蹈风格");
+                }
+                if (styleMap.get("letter") != null) {
+                    itemData.add(styleMap.get("letter"));
+                }else{
+                    itemData.add("没有填首字母");
+                }
+                printer.printRecord(itemData);
+            }
+            //close the printer
+            printer.flush();
+            printer.close();
+            //out.flush();
+            //out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping(value = "downloadmusics", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> downloadmusics() throws IOException {
+        writeMusicsToCSV();
+
+        String path = "music.csv";
+        File file = new File(path);
+        HttpHeaders headers = new HttpHeaders();
+        String fileName = new String("music.csv".getBytes("UTF-8"), "iso-8859-1");//为了解决中文名称乱码问题
         headers.setContentDispositionFormData("attachment", fileName);
         headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
         return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),
--- src/test/java/com/springapp/mvc/DownloadTests.java
+++ src/test/java/com/springapp/mvc/DownloadTests.java
@@ -4,9 +4,10 @@ package com.springapp.mvc;
  * Created by zjh on 14-12-11.
  */
 
+import com.darfoo.backend.dao.EducationDao;
+import com.darfoo.backend.dao.MusicDao;
 import com.darfoo.backend.dao.VideoDao;
-import com.darfoo.backend.model.Video;
-import com.darfoo.backend.model.VideoCategory;
+import com.darfoo.backend.model.*;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.junit.Test;
@@ -28,6 +29,10 @@ import java.util.Set;
 public class DownloadTests {
     @Autowired
     VideoDao videoDao;
+    @Autowired
+    EducationDao educationDao;
+    @Autowired
+    MusicDao musicDao;
 
     @Test
     public void writeVideosToCSV(){
@@ -59,34 +64,155 @@ public class DownloadTests {
                         System.out.println("something is wrong with the category");
                     }
                 }
-                List<String> videoData = new ArrayList<String>();
-                videoData.add(video.getTitle());
+                List<String> itemData = new ArrayList<String>();
+                itemData.add(video.getTitle());
                 if (video.getAuthor() != null) {
-                    videoData.add(video.getAuthor().getName());
+                    itemData.add(video.getAuthor().getName());
                 }else{
-                    videoData.add("没有关联明星舞队");
+                    itemData.add("没有关联明星舞队");
                 }
                 if (styleMap.get("speed") != null) {
-                    videoData.add(styleMap.get("speed"));
+                    itemData.add(styleMap.get("speed"));
                 }else{
-                    videoData.add("没有填视频速度");
+                    itemData.add("没有填视频速度");
                 }
                 if (styleMap.get("difficult") != null) {
-                    videoData.add(styleMap.get("speed"));
+                    itemData.add(styleMap.get("difficult"));
                 }else{
-                    videoData.add("没有填视频速度");
+                    itemData.add("没有填视频难度");
                 }
                 if (styleMap.get("style") != null) {
-                    videoData.add(styleMap.get("speed"));
+                    itemData.add(styleMap.get("style"));
                 }else{
-                    videoData.add("没有填视频速度");
+                    itemData.add("没有填视频风格");
                 }
                 if (styleMap.get("letter") != null) {
-                    videoData.add(styleMap.get("speed"));
+                    itemData.add(styleMap.get("letter"));
                 }else{
-                    videoData.add("没有填视频速度");
+                    itemData.add("没有填首字母");
                 }
-                printer.printRecord(videoData);
+                printer.printRecord(itemData);
+            }
+            //close the printer
+            printer.flush();
+            printer.close();
+            //out.flush();
+            //out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void writeTutorialsToCSV(){
+        List<Education> educations = educationDao.getAllEducation();
+        CSVFormat format = CSVFormat.RFC4180.withHeader().withDelimiter(',');
+        CSVPrinter printer = null;
+        try {
+            Writer out = new FileWriter("tutorial.csv");
+            printer = new CSVPrinter(out, format.withDelimiter(','));
+            System.out.println("********");
+            printer.printRecord("教程标题", "明星舞队名称", "舞蹈速度", "舞蹈难度", "舞蹈风格");
+            for(Education tutorial : educations){
+                HashMap<String, String> styleMap = new HashMap<String, String>();
+                Set<EducationCategory> categories = tutorial.getCategories();
+                for (EducationCategory category : categories){
+                    int categoryid = category.getId();
+                    String categorytitle = category.getTitle();
+                    System.out.println(categoryid);
+                    System.out.println(categorytitle);
+                    if (categorytitle.equals("快") || categorytitle.equals("中") || categorytitle.equals("慢")){
+                        styleMap.put("speed", categorytitle);
+                    }else if(categorytitle.equals("简单") || categorytitle.equals("适中") || categorytitle.equals("稍难")){
+                        styleMap.put("difficult", categorytitle);
+                    }else if(categorytitle.equals("背面教学") || categorytitle.equals("分解教学") || categorytitle.equals("队形表演")){
+                        styleMap.put("style", categorytitle);
+                    }else{
+                        System.out.println("something is wrong with the category");
+                    }
+                }
+                List<String> itemData = new ArrayList<String>();
+                itemData.add(tutorial.getTitle());
+                if (tutorial.getAuthor() != null) {
+                    itemData.add(tutorial.getAuthor().getName());
+                }else{
+                    itemData.add("没有关联明星舞队");
+                }
+                if (styleMap.get("speed") != null) {
+                    itemData.add(styleMap.get("speed"));
+                }else{
+                    itemData.add("没有填教程速度");
+                }
+                if (styleMap.get("difficult") != null) {
+                    itemData.add(styleMap.get("difficult"));
+                }else{
+                    itemData.add("没有填教程难度");
+                }
+                if (styleMap.get("style") != null) {
+                    itemData.add(styleMap.get("style"));
+                }else{
+                    itemData.add("没有填教程风格");
+                }
+
+                printer.printRecord(itemData);
+            }
+            //close the printer
+            printer.flush();
+            printer.close();
+            //out.flush();
+            //out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void writeMusicsToCSV(){
+        List<Music> musics = musicDao.getAllMusic();
+        CSVFormat format = CSVFormat.RFC4180.withHeader().withDelimiter(',');
+        CSVPrinter printer = null;
+        try {
+            Writer out = new FileWriter("music.csv");
+            printer = new CSVPrinter(out, format.withDelimiter(','));
+            System.out.println("********");
+            printer.printRecord("伴奏标题", "舞蹈节奏", "舞蹈风格", "首字母");
+            for(Music music : musics){
+                HashMap<String, String> styleMap = new HashMap<String, String>();
+                Set<MusicCategory> categories = music.getCategories();
+                for (MusicCategory category : categories){
+                    int categoryid = category.getId();
+                    String categorytitle = category.getTitle();
+                    System.out.println(categoryid);
+                    System.out.println(categorytitle);
+                    if (categoryid >= 1 && categoryid <= 4){
+                        styleMap.put("beat", categorytitle);
+                    }else if(categoryid >= 5 && categoryid <= 12){
+                        styleMap.put("style", categorytitle);
+                    }else if(categoryid >= 13 && categoryid <= 38){
+                        styleMap.put("letter", categorytitle);
+                    }else{
+                        System.out.println("something is wrong with the category");
+                    }
+                }
+                List<String> itemData = new ArrayList<String>();
+                itemData.add(music.getTitle());
+
+                if (styleMap.get("beat") != null) {
+                    itemData.add(styleMap.get("beat"));
+                }else{
+                    itemData.add("没有填舞蹈节奏");
+                }
+                if (styleMap.get("style") != null) {
+                    itemData.add(styleMap.get("style"));
+                }else{
+                    itemData.add("没有填舞蹈风格");
+                }
+                if (styleMap.get("letter") != null) {
+                    itemData.add(styleMap.get("letter"));
+                }else{
+                    itemData.add("没有填首字母");
+                }
+                printer.printRecord(itemData);
             }
             //close the printer
             printer.flush();
