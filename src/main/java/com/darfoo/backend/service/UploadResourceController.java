9d0bda90ee730dca70a5b2b0a1c69a3ac0c30408
zjh <zjhsdtc@gmail.com>
2014-12-22 17:59:18 +0800

    finish callback request after upload resource to qiniu cloud

--- src/main/java/com/darfoo/backend/service/UploadResourceController.java
+++ src/main/java/com/darfoo/backend/service/UploadResourceController.java
@@ -1,11 +1,14 @@
 package com.darfoo.backend.service;
 
+import com.darfoo.backend.service.responsemodel.UploadStatus;
 import com.darfoo.backend.service.responsemodel.UploadToken;
 import com.darfoo.backend.utils.QiniuUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by zjh on 14-12-22.
@@ -27,5 +30,19 @@ public class UploadResourceController {
     UploadToken getUploadToken(){
         String token = qiniuUtils.getToken();
         return new UploadToken(token);
+    }
+
+    /**
+     * 当客户端成功将资源上传至七牛之后需要请求这个回调请求，将资源有关信息发送给服务器数据库
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "finishcallback", method = RequestMethod.POST)
+    public @ResponseBody
+    UploadStatus uploadFinishCallback(HttpServletRequest request){
+        System.out.println("post title -> " + request.getParameter("title"));
+        System.out.println("post authorid -> " + request.getParameter("authorid"));
+
+        return new UploadStatus("ok");
     }
 }
