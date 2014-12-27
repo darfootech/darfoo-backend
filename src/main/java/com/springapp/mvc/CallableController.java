1a4d34b3fc0f3ad980afce364cdd7d6996bbb7c4
zjh <zjh@cleantha.local>
2014-12-27 13:12:27 +0800

    deferredresult需要入队列，还需要用一个定时器去定时放入结果，感觉有点坑 还是使用callable这种async的方式感觉靠谱一点

---
+++ src/main/java/com/springapp/mvc/CallableController.java
@@ -0,0 +1,32 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+/**
+ * Created by zjh on 14-12-27.
+ */
+
+@Controller
+@RequestMapping("/testasync")
+public class CallableController {
+    @RequestMapping("/responsebody")
+    public @ResponseBody
+    Callable<Map<String, Object>> callable() {
+
+        return new Callable<Map<String, Object>>() {
+            @Override
+            public Map<String, Object> call() throws Exception {
+                //Thread.sleep(2000);
+                Map<String, Object> result = new HashMap<String, Object>();
+                result.put("status", "cleantha");
+                return result;
+            }
+        };
+    }
+}
