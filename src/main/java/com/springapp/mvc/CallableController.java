ecfb46c488d7783313688b03d05742526c58feeb
zjh <zjh@cleantha.local>
2014-12-27 21:24:45 +0800

    schedule可以作为定时清理缓存session的作用存在 async大法好，负载还没有测，如果效果拔群后面全部都要改成异步的controller

--- src/main/java/com/springapp/mvc/CallableController.java
+++ src/main/java/com/springapp/mvc/CallableController.java
@@ -18,15 +18,23 @@ public class CallableController {
     @RequestMapping("/responsebody")
     public @ResponseBody
     Callable<Map<String, Object>> callable() {
-
         return new Callable<Map<String, Object>>() {
             @Override
             public Map<String, Object> call() throws Exception {
+                //just did the block stuff and it's asynchronize
                 //Thread.sleep(2000);
                 Map<String, Object> result = new HashMap<String, Object>();
                 result.put("status", "cleantha");
                 return result;
             }
         };
+    }
+
+    @RequestMapping("/normalresponse")
+    public @ResponseBody
+    Map<String, Object> normalresponse() {
+        Map<String, Object> result = new HashMap<String, Object>();
+        result.put("status", "cleantha");
+        return result;
     }
 }
