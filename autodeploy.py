e80d3c6265efb3950d882528c4d997e5ccf45d67
jihui <jihui@darfoo.com>
2015-01-26 11:48:46 +0800

    finish auto redeploy use fabric

--- autodeploy.py
+++ autodeploy.py
@@ -1,4 +1,5 @@
 from fabric.api import *
+from fabric.colors import green
 
 env.roledefs = {
     'backendserver': ['root@112.124.68.27']
@@ -6,18 +7,25 @@ env.roledefs = {
 
 backend_project_dir = '/home/darfoo-backend'
 war_dir = '/usr/local/darfoo/webapps/darfoobackend'
+tomcat_dir = '/usr/local/darfoo/apache-tomcat-7.0.47/bin'
 tomcat_one_dir = '/usr/local/darfoo/tomcat_bak1/bin/'
 tomcat_two_dir = '/usr/local/darfoo/tomcat_bak2/bin/'
 
 @roles('backendserver')
-def killTomcatTask():
-    with cd(backend_project_dir):
-        run('git pull --rebase upstream release')
-        run("./killtomcats.sh")
+def shutdownTomcats():
+    with cd(tomcat_dir):
+        run('./shutdown.sh')
+    with cd(tomcat_one_dir):
+        run('./shutdown.sh')
+    with cd(tomcat_two_dir):
+        run('./shutdown.sh')
+
+    print green("shutdown all tomcats")
 
 @roles('backendserver')
 def updateCodeAndRebuild():
     with cd(backend_project_dir):
+        run('git pull --rebase upstream release')
         run('rm -rf target/')
         run('mvn package -Dmaven.test.skip=true')
         run('rm -rf /usr/local/darfoo/webapps/darfoobackend')
@@ -28,15 +36,21 @@ def updateCodeAndRebuild():
     with cd(war_dir):
         run('jar -xvf darfoobackend.war')
         run('rm darfoobackend.war')
+
+    print green("updatecodes and rebuild")
         
 @roles('backendserver')
 def deploy():
-    with cd(backend_project_dir):
-        run('catalina.sh run')
+    with cd(tomcat_dir):
+        run('./startup.sh')
     with cd(tomcat_one_dir):
-        run('./catalina.sh run')
+        run('./startup.sh')
     with cd(tomcat_two_dir):
-        run('./catalina.sh run')
+        run('./startup.sh')
+
+    print green("deploy success")
 
 def backend():
-    execute(killTomcatTask)
+    execute(shutdownTomcats)
+    execute(updateCodeAndRebuild)
+    execute(deploy)
