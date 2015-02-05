5318e52a10fa7f10f33f561b977c44b75ad49649
jihui <jihui@darfoo.com>
2015-02-05 21:26:15 +0800

    降低服务器权限之后 重新配置数据库备份脚本

--- databasebackup.sh
+++ databasebackup.sh
@@ -1,4 +1,4 @@
-ssh root@112.124.68.27   'cd /home/darfoodatabase; \
+ssh -p 33333 oofrad@112.124.68.27   'cd /home/oofrad/darfoodatabase; \
                           mysqldump --extended-insert=FALSE --complete-insert=TRUE -uroot -p darfoo > darfoo.sql; \
                           git commit -a -m"backupdatabase";\
                           git push'
