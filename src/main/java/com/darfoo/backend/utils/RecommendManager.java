c8eae410e0fd98ec919efd471b2777b86bd8762f
jihui <jihui@darfoo.com>
2015-02-12 17:17:26 +0800

    fix can not initialize recommendmanager bug cause now use database to specify recommended video and tutorial but not config files any more

--- src/main/java/com/darfoo/backend/utils/RecommendManager.java
+++ src/main/java/com/darfoo/backend/utils/RecommendManager.java
@@ -10,8 +10,9 @@ public class RecommendManager {
     public static Config recommendConfig;
     public static String basepath;
 
-    static {
+    //we just use database to specific recommend video ant tutorial and do not need configfactory any more
+    /*static {
         recommendConfig = ConfigFactory.load("recommend");
         basepath = recommendConfig.getString("recommend.basepath");
-    }
+    }*/
 }
