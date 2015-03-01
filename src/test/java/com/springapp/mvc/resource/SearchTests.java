5e5d54275289bb3e5e6d56b4daca5fcefc4715cd
jihui <jihui@darfoo.com>
2015-03-01 10:59:01 +0800

    fix errors in resource search tests

--- src/test/java/com/springapp/mvc/resource/SearchTests.java
+++ src/test/java/com/springapp/mvc/resource/SearchTests.java
@@ -1,9 +1,7 @@
 package com.springapp.mvc.resource;
 
 import com.darfoo.backend.caches.client.CommonRedisClient;
-import com.darfoo.backend.caches.dao.AuthorCacheDao;
-import com.darfoo.backend.caches.dao.MusicCacheDao;
-import com.darfoo.backend.caches.dao.TutorialCacheDao;
+import com.darfoo.backend.caches.dao.CacheDao;
 import com.darfoo.backend.caches.dao.VideoCacheDao;
 import com.darfoo.backend.dao.cota.CommonDao;
 import com.darfoo.backend.dao.resource.AuthorDao;
@@ -30,17 +28,13 @@ import java.util.Set;
         "file:src/main/webapp/WEB-INF/redis-context.xml",
         "file:src/main/webapp/WEB-INF/springmvc-hibernate.xml"
 })
-public class SearchDaoTests {
+public class SearchTests {
     @Autowired
     VideoCacheDao videoCacheDao;
     @Autowired
-    TutorialCacheDao tutorialCacheDao;
-    @Autowired
-    MusicCacheDao musicCacheDao;
-    @Autowired
     AuthorDao authorDao;
     @Autowired
-    AuthorCacheDao authorCacheDao;
+    CacheDao cacheDao;
     @Autowired
     CommonRedisClient redisClient;
     @Autowired
@@ -94,7 +88,7 @@ public class SearchDaoTests {
         for (Video video : videos) {
             int vid = video.getId();
             long status = redisClient.sadd("videosearch" + searchContent, "video-" + vid);
-            videoCacheDao.insertSingleVideo(video);
+            cacheDao.insertSingleResource(Video.class, video, "video");
             System.out.println("insert result -> " + status);
         }
 
@@ -102,8 +96,7 @@ public class SearchDaoTests {
         List<SingleVideo> result = new ArrayList<SingleVideo>();
         for (String key : searchVideoKeys) {
             System.out.println("key -> " + key);
-            int vid = Integer.parseInt(key.split("-")[1]);
-            SingleVideo video = videoCacheDao.getSingleVideo(vid);
+            SingleVideo video = (SingleVideo) cacheDao.getSingleResource(SingleVideo.class, key);
             System.out.println("title -> " + video.getTitle());
             result.add(video);
         }
@@ -120,15 +113,14 @@ public class SearchDaoTests {
         for (Tutorial video : videos) {
             int vid = video.getId();
             long status = redisClient.sadd("tutorialsearch" + searchContent, "tutorial-" + vid);
-            tutorialCacheDao.insertSingleTutorial(video);
+            cacheDao.insertSingleResource(Tutorial.class, video, "tutorial");
             System.out.println("insert result -> " + status);
         }
 
         Set<String> searchTutorialKeys = redisClient.smembers("tutorialsearch" + searchContent);
         for (String key : searchTutorialKeys) {
             System.out.println("key -> " + key);
-            int tid = Integer.parseInt(key.split("-")[1]);
-            SingleVideo tutorial = tutorialCacheDao.getSingleTutorial(tid);
+            SingleVideo tutorial = (SingleVideo) cacheDao.getSingleResource(SingleVideo.class, key);
             System.out.println("title -> " + tutorial.getTitle());
             result.add(tutorial);
         }
@@ -145,15 +137,14 @@ public class SearchDaoTests {
         for (Music music : musics) {
             int mid = music.getId();
             long status = redisClient.sadd("musicsearch" + searchContent, "music-" + mid);
-            musicCacheDao.insertSingleMusic(music);
+            cacheDao.insertSingleResource(Music.class, music, "music");
             System.out.println("insert result -> " + status);
         }
 
         Set<String> searchMusicKeys = redisClient.smembers("musicsearch" + searchContent);
         for (String key : searchMusicKeys) {
             System.out.println("key -> " + key);
-            int mid = Integer.parseInt(key.split("-")[1]);
-            SingleMusic music = musicCacheDao.getSingleMusic(mid);
+            SingleMusic music = (SingleMusic) cacheDao.getSingleResource(SingleMusic.class, key);
             System.out.println("title -> " + music.getTitle());
             result.add(music);
         }
@@ -170,15 +161,14 @@ public class SearchDaoTests {
         for (Author author : authors) {
             int aid = author.getId();
             long status = redisClient.sadd("authorsearch" + searchContent, "author-" + aid);
-            authorCacheDao.insertSingleAuthor(author);
+            cacheDao.insertSingleResource(Author.class, author, "author");
             System.out.println("insert result -> " + status);
         }
 
         Set<String> searchAuthorKeys = redisClient.smembers("authorsearch" + searchContent);
         for (String key : searchAuthorKeys) {
             System.out.println("key -> " + key);
-            int aid = Integer.parseInt(key.split("-")[1]);
-            SingleAuthor author = authorCacheDao.getSingleAuthor(aid);
+            SingleAuthor author = (SingleAuthor) cacheDao.getSingleResource(Author.class, key);
             System.out.println("name -> " + author.getName());
             result.add(author);
         }
