3366b37b40f04d17acfa6cf0d709ae9b84ee7501
zjh <zjh@cleantha.local>
2015-03-25 13:25:59 +0800

    start to merge video and tutorial into dancevideo model 因为后面还有越剧这货 考虑横向扩展

--- src/main/java/com/darfoo/backend/model/cota/AuthorHot.java
+++ src/main/java/com/darfoo/backend/model/cota/AuthorHot.java
@@ -3,6 +3,7 @@ package com.darfoo.backend.model.cota;
 /**
  * Created by zjh on 15-3-23.
  */
+//一个舞队是否为热门舞队
 public enum AuthorHot {
     NOTHOT, ISHOT
 }
--- src/main/java/com/darfoo/backend/model/cota/ModelInsert.java
+++ src/main/java/com/darfoo/backend/model/cota/ModelInsert.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
  * Created by zjh on 15-2-23.
  */
 
-//标识出资源中需要从界面view层获得并插入到数据库的字段名称
+//标识出资源中需要从界面view层获得并插入到数据库的字段名称 也就是从后台界面中传入的field的名字 并不用插入数据库中
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface ModelInsert {
